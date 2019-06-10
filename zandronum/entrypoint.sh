89e2d27b75dd17222549427723c62598d419b01b
Leon Grünewald <l_gruenewald@web.de>
2019-06-10 19:32:29 +0200

    Try to fix startup script once more

--- zandronum/entrypoint.sh
+++ zandronum/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/container
 
+if [[ ! -z ${WADS} ]]; then
+    MODIFIED_STARTUP="$STARTUP -file $WADS"
+else
+    MODIFIED_STARTUP="$STARTUP"
+fi
+
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
