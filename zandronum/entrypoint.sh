51f1e0566a731fd579b7e6187a4305d78e4196b1
Leon Grünewald <leon.gruenewald@kreativrudel.de>
2021-05-12 00:22:33 +0200

    Output realpath

--- zandronum/entrypoint.sh
+++ zandronum/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /home/container
-mkdir -p .config/zandronum
-
+mkdir -p ~/.config/zandronum
+echo $(realpath ~/.config)
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
