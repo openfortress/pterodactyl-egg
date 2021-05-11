9304e8602a52c1cc65fa1402592ab87a7078c644
Leon Grünewald <leon.gruenewald@kreativrudel.de>
2021-05-12 01:01:52 +0200

    Fix up user

--- zandronum/entrypoint.sh
+++ zandronum/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd /home/container
-mkdir -p ~/.config/zandronum
-echo $(realpath ~/.config)
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
