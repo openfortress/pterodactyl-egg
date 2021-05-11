14b401127728fd0da77c92744aa9ca9d5d741b58
Leon Grünewald <leon.gruenewald@kreativrudel.de>
2021-05-12 00:19:10 +0200

    Make .config in entrypoint

--- zandronum/entrypoint.sh
+++ zandronum/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/container
+mkdir -p .config/zandronum
+
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
